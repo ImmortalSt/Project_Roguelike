3b9eb319bc16f46634933a46098a8be6aad17de8
N1K0L41M4 <120606632+MasalovNIkolai@users.noreply.github.com>
2023-03-26 23:56:19 +1000

    Convert Main methods of Class Field to functions, Add Function of Converting Elements of Field to Strings

--- src/components/utills/utils.cpp
+++ src/components/utills/utils.cpp
@@ -21,3 +21,60 @@ void Clear()
     system("clear");
 #endif
 }
+
+void FillSquare(Field* _field, int h1, int w1, int h2, int w2, CellState _state) {
+
+	if ((h1 > h2) || (w1 > w2)) {
+		throw "Error! Incorrect values! The First Two are the Upper Left corner; The Second Two are the Lower Left corner!";
+	}
+
+	if ((h1 > _field->GetHeight() || h2 > _field->GetHeight()) || (w1 > _field->GetWidth() || w2 > _field->GetWidth())) {
+		throw "You set Incorrect values! One of values is higher than limit of the Field";
+	}
+
+	if (h1 < 0 || h2 < 0 || w1 < 0 || w2 < 0) {
+		throw "All values must be 0 or higher";
+	}
+
+	for (int i = h1; i <= h2; i++) {
+		for (int j = w1; j <= w2; j++) {
+			_field->SetCell(i, j, _state);
+		}
+	}
+};
+
+Field GetTestField() {
+	Field _field = Field(7, 12);
+	FillSquare(&_field, 0, 0, 6, 11, wall);
+	FillSquare(&_field, 1, 1, 5, 10, pass);
+	_field.SetCell(2, 2, player);
+	_field.SetCell(2, 8, enemy);
+	_field.SetCell(4, 8, shop);
+	return _field;
+};
+
+void PrintField(Field _field) {
+	for (int i = 0; i < _field.GetHeight(); i++) {
+		for (int j = 0; j < _field.GetWidth(); j++) {
+			std::cout << _field.GetCellChar(i, j);
+		}
+		std::cout << std::endl;
+	}
+};
+
+vector<string> RebuildFieldInStrings(Field _field) {
+	vector<string> field;
+	field.resize(_field.GetHeight());
+	for (int i = 0; i < _field.GetHeight(); i++) {
+		for (int j = 0; j < _field.GetWidth(); j++) {
+			field[i] += _field.GetCellChar(i, j);
+		}
+	}
+	return field;
+};
+
+void PrintFieldInStrings(vector<string> _field) {
+	for (int i = 0; i < _field.size(); i++) {
+		cout << _field[i] << std::endl;
+	}
+};
--- src/components/utills/utils.h
+++ src/components/utills/utils.h
@@ -5,9 +5,23 @@
 #include <string>
 #include <filesystem>
 #include <Windows.h>
+#include <vector>
+#include <string>
+#include "../Field/Field.h"
+using namespace std;
 
 std::wstring repeat(std::wstring a, unsigned int b);
 
 void Clear();
+
+void FillSquare(Field* _field, int h1, int w1, int h2, int w2, CellState _state);
+
+Field GetTestField();
+
+void PrintField(Field _field);
+
+vector<string> RebuildFieldInStrings(Field _field);
+
+void PrintFieldInStrings(vector<string> _field);
 
 #endif
