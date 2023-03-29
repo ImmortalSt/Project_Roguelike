838eae9c7d35c5c4f85e66052651f8bdb534342d
aerzex <113291689+aerzex@users.noreply.github.com>
2023-03-29 20:32:41 +1000

    Add function for Map_gen in Utils.cpp

--- src/components/utills/utils.cpp
+++ src/components/utills/utils.cpp
@@ -28,7 +28,7 @@ void FillSquare(Field* _field, int h1, int w1, int h2, int w2, CellState _state)
 		throw "Error! Incorrect values! The First Two are the Upper Left corner; The Second Two are the Lower Left corner!";
 	}
 
-	if ((h1 > _field->GetHeight() || h2 > _field->GetHeight()) || (w1 > _field->GetWidth() || w2 > _field->GetWidth())) {
+	if ((h1 >= _field->GetHeight() || h2 >= _field->GetHeight()) || (w1 >= _field->GetWidth() || w2 >= _field->GetWidth())) {
 		throw "You set Incorrect values! One of values is higher than limit of the Field";
 	}
 
@@ -78,3 +78,64 @@ void PrintFieldInStrings(vector<string> _field) {
 		cout << _field[i] << std::endl;
 	}
 };
+
+const int* Massive_of_rand_l() {
+	int l1 = 10 + rand() % 11;
+	int l2 = 20 + rand() % 21;
+	int l3 = 25 + rand() % 50;
+	int l4 = 134 - (l1 + l2 + l3);
+	int* arr = new int[4];
+	arr[0] = l1;
+	arr[1] = l2;
+	arr[2] = l3;
+	arr[3] = l4;
+	if ((l1 + l2 + l3 + l4) == 134) {
+		return arr;
+	}
+	else {
+		std::cout << "Oops! Some error in function \"Massive_of_rand_l\"" << std::endl;
+	}
+}
+
+const int* Massive_of_rand_h() {
+	int h1 = 5 + rand() % 3;
+	int h2 = 4 + rand() % 5;
+	int h3 = 22 - h1 - h2;
+	int* arr = new int[3];
+	arr[0] = h1;
+	arr[1] = h2;
+	arr[2] = h3;
+	if (h1 + h2 + h3 == 22) {
+		return arr;
+	}
+	else {
+		std::cout << "Oops! Some error in function \"Massive_of_rand_h\"" << std::endl;
+		return arr;
+	}
+}
+
+Field* Make_border(const int* arr_h, const int* arr_l) {
+	int a = 0, b = 0;
+	for (int i = 0; i < 4; i++)
+		a += arr_l[i];
+	for (int i = 0; i < 3; i++)
+		b += arr_h[i];
+	Field* mafield = new Field(b, a);
+	return mafield;
+}
+
+int* rand_arr(const int a) {
+	int* arr = new int[a];
+	arr[0] = 1 + rand() % 12;
+	for (int i = 0; i < a + 1; i++) {
+		arr[i + 1] = 1 + rand() % 12;
+		if (arr[i + 1] != arr[i]) {
+
+		}
+		else {
+			i = 0;
+		}
+
+	}
+	return arr;
+}
