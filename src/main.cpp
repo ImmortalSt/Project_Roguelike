a2fc342e1329015cc0874c5cbad9f398438a31df
etr1x <etr1x@gmail.com>
2023-04-07 00:05:29 +1000

    Added mario_theme on start

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,41 @@
 #include <time.h>
 #include "components/Scene/BattleScene.h"
 #include "components/shop/shop.h"
+#include "components/Sounds/sounds.h"
+#include <thread>
 
+void mario_theme() {
+    Beep(330, 100); Sleep(100);
+    Beep(330, 100); Sleep(300);
+    Beep(330, 100); Sleep(300);
+    Beep(262, 100); Sleep(100);
+    Beep(330, 100); Sleep(300);
+    Beep(392, 100); Sleep(700);
+    Beep(196, 100); Sleep(700);
+    Beep(262, 300); Sleep(300);
+    Beep(196, 300); Sleep(300);
+    Beep(164, 300); Sleep(300);
+    Beep(220, 300); Sleep(100);
+    Beep(246, 100); Sleep(300);
+    Beep(233, 200);
+    Beep(220, 100); Sleep(300);
+    Beep(196, 100); Sleep(150);
+    Beep(330, 100); Sleep(150);
+    Beep(392, 100); Sleep(150);
+    Beep(440, 100); Sleep(300);
+    Beep(349, 100); Sleep(100);
+    Beep(392, 100); Sleep(300);
+    Beep(330, 100); Sleep(300);
+    Beep(262, 100); Sleep(100);
+    Beep(294, 100); Sleep(100);
+    Beep(247, 100); Sleep(500);
+    Beep(262, 300); Sleep(300);
+    Beep(196, 300); Sleep(300);
+    Beep(164, 300); Sleep(300);
+    Beep(220, 300); Sleep(100);
+    Beep(246, 100); Sleep(300);
+    Beep(233, 200);
+}
 
 BOOL ShowConsoleCursor(BOOL bShow)
 {
@@ -31,6 +65,11 @@ BOOL ShowConsoleCursor(BOOL bShow)
     return TRUE;
 }
 
+void play() {
+    std::thread th(mario_theme);
+    th.detach();
+}
+
 int main() {
     srand(time(0));
     setlocale(LC_ALL, "");
@@ -38,6 +77,8 @@ int main() {
     ShowConsoleCursor(FALSE);
     ShowCursor(FALSE);
     _setmode(_fileno(stdout), _O_U16TEXT);
+    
+    play();
 
     Engine* engine = new Engine();
     Inventory inventory(std::vector<Item*>(), 500);
